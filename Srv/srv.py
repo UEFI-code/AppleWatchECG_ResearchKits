45844db4c4e54df342a8b54d1a6eab5a78f9053e
= <=>
2023-11-03 21:45:01 +0800

    Bug Fix

--- Srv/srv.py
+++ Srv/srv.py
@@ -1,14 +1,21 @@
 from flask import Flask, request
 import socket
+import sys
+import os
+import json
 
 app = Flask(__name__)
 
 @app.route('/data', methods=['POST'])
-def receive_data():
-    data = request.get_json()
-    print(data)
-    # Do something with the data
-    return 'Data received'
+def data():
+    data = request.get_data()
+    try:
+        json_data = json.loads(data)
+        print(json_data)
+    except:
+        print('Invalid JSON')
+        return 'Invalid JSON', 400
+    return 'OK', 200
 
 if __name__ == '__main__':
     # get our ipv4 address in the local network
@@ -16,4 +23,5 @@ if __name__ == '__main__':
     ip = socket.gethostbyname(hostname)
     print('Server IP: ', ip)
     print('Server Port: 5000')
+    # run the server
     app.run(host=ip, port=5000, debug=True)
